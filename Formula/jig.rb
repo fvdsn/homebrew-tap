1b48f9acfabaab344262e33c685167eb91cf0848
Frédéric van der Essen <fvdessen@gmail.com>
2026-08-12 12:59:01 +0200

    Bump jig to 1.12.0

--- Formula/jig.rb
+++ Formula/jig.rb
@@ -1,28 +1,28 @@
 class Jig < Formula
   desc "Manage a workspace of many Git repositories from a single shared schema"
   homepage "https://github.com/fvdsn/jig"
-  version "1.11.0"
+  version "1.12.0"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/fvdsn/jig/releases/download/v1.11.0/jig_v1.11.0_darwin_arm64.tar.gz"
-      sha256 "d44884cf504c3e15df0b58dfb4a2803e3bca47aeabe93f738d9cbcfae147fcd9"
+      url "https://github.com/fvdsn/jig/releases/download/v1.12.0/jig_v1.12.0_darwin_arm64.tar.gz"
+      sha256 "d96d4fde96ac781715c59a7005c4c2711fa8bbf438da8d95c0d712f4699127c2"
     end
     on_intel do
-      url "https://github.com/fvdsn/jig/releases/download/v1.11.0/jig_v1.11.0_darwin_amd64.tar.gz"
-      sha256 "a7c23625e621d900b40992af98e064875058fa837477de6a8f04edc005441681"
+      url "https://github.com/fvdsn/jig/releases/download/v1.12.0/jig_v1.12.0_darwin_amd64.tar.gz"
+      sha256 "64ad189a4d5a3d6554accf71c338dd4bf9105e426ff72d88ab4912dc88c30fa5"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/fvdsn/jig/releases/download/v1.11.0/jig_v1.11.0_linux_arm64.tar.gz"
-      sha256 "1d20f3a61416d7b6cb936c104a21d241e1296e5b444dfbb6d891b2761d891b3e"
+      url "https://github.com/fvdsn/jig/releases/download/v1.12.0/jig_v1.12.0_linux_arm64.tar.gz"
+      sha256 "924f067d8e65cdf32425511564f504f8195f36ff87ef2153e74275dcefb0f982"
     end
     on_intel do
-      url "https://github.com/fvdsn/jig/releases/download/v1.11.0/jig_v1.11.0_linux_amd64.tar.gz"
-      sha256 "b0982b7ca9b48de25e937089586c5fd66a5b18940e42a8880a6db229ed399b8f"
+      url "https://github.com/fvdsn/jig/releases/download/v1.12.0/jig_v1.12.0_linux_amd64.tar.gz"
+      sha256 "862aeaf5aff12cab5fa2b9948f23ba30c987408d6a7682a667604278f8aca683"
     end
   end
 
@@ -31,6 +31,6 @@ class Jig < Formula
   end
 
   test do
-    assert_match "jig v1.11.0", shell_output("#{bin}/jig --version")
+    assert_match "jig v1.12.0", shell_output("#{bin}/jig --version")
   end
 end
